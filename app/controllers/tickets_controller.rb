a4dd2743ed5648d713d005f9f93f459e16038eda
Tim Riley <tim@openmonkey.com>
2008-09-07 00:56:40 +1000

    change ActiveResource models to use the /api/v1 URL and add an index action to tickets controller.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,12 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.find(:all)
+    
+    respond_to do |format|
+      format.html
+    end
+  end
+  
   def show
     @ticket = Ticket.find_by_number(params[:id])
     
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,14 @@
 class Ticket < ActiveResource::Base
   
-  self.site = "https://#{APP_CONFIG['unfuddle_username']}:#{APP_CONFIG['unfuddle_password']}@#{APP_CONFIG['unfuddle_subdomain']}.unfuddle.com/projects/#{APP_CONFIG['unfuddle_project_id']}/"
+  self.site = "https://#{APP_CONFIG['unfuddle_username']}:#{APP_CONFIG['unfuddle_password']}@#{APP_CONFIG['unfuddle_subdomain']}.unfuddle.com/api/v1/projects/#{APP_CONFIG['unfuddle_project_id']}/"
   
   def self.find_by_number(number)
     find(:one, :from => "/projects/#{APP_CONFIG['unfuddle_project_id']}/tickets/by_number/#{number}.xml")
+  end
+  
+  # We use the ticket number (not the ID) in the URL, since the number is more meaningful.
+  def to_param
+    number
   end
   
   def comments
